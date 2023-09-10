8b822b68c279ec15365ffb92ba3b5179281a1f4e
ImpacTusdevloper <krishkushwaha99999@gmail.com>
2023-09-10 17:28:29 +0530

    Alpha 1.0
    Basic cinematic shot of simulation

--- Assets/Scripts/Elements/RandomColor.cs
+++ Assets/Scripts/Elements/RandomColor.cs
@@ -1,25 +1,22 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class RandomColor : MonoBehaviour
 {
+    public bool bright = false;
     // Start is called before the first frame update
     void Start()
     {
-        
         Material mat = GetComponentInChildren<MeshRenderer>().material;
-
-        Color col = new Color();
-        
-        col.r = Random.Range(0f, 1f);
-        col.g = Random.Range(0f, 1f);
-        col.b = Random.Range(0f, 1f);
-        float h;
-        float s;
-        float v;
-        Color.RGBToHSV(col, out h, out s, out v);
-        v = 1f;
-        mat.color = col;
+        if(bright)
+        {
+            Color col = Random.ColorHSV(0f, 1f, 0.8f, 1f, 1f, 1f);
+            mat.color = col;
+        }
+        else
+        {
+            Color col = Random.ColorHSV();
+            mat.color = col;
+        }
 
     }
 }
